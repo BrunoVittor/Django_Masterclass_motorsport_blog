9cfa485121473716856156aff257b05173ea9302
BrunoVittor <brunohapk@hotmail.com>
2022-06-01 18:59:03 -0300

    categories now implemented

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
 	path('', IndexView.as_view(), name='index'),
 	path('details/<int:pk>/<slug:slug>', PostDetailView.as_view(), name='details'),
+	path('category/<int:pk>/<slug:slug>', CategoryDetailView.as_view(), name='category_details'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,24 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView, ListView, DetailView
 from .models import SportCars, Category
 
 
 class IndexView(ListView):
-	template_name = 'posts/index.html'
-	model = SportCars
-	context_object_name = 'posts'
+    template_name = 'posts/index.html'
+    model = SportCars
+    context_object_name = 'posts'
 
-	def get_context_data(self, *, object_list=None, **kwargs):
-		context = super(IndexView, self).get_context_data(**kwargs)
-		context['categories'] = Category.objects.all()
-		return context
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
 
 class PostDetailView(DetailView):
-	template_name = 'posts/detail.html'
-	model = SportCars
-	context_object_name = 'detail_view'
+    template_name = 'posts/detail.html'
+    model = SportCars
+    context_object_name = 'detail_view'
 
-	def get_context_data(self, **kwargs):
-		context = super(PostDetailView, self).get_context_data(**kwargs)
-		return context
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        return context
+
+
+class CategoryDetailView(ListView):
+    model = SportCars
+    template_name = 'categories/category_detail.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, pk=self.kwargs['pk'])
+        return SportCars.objects.filter(category=self.category).order_by('-id')
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryDetailView, self).get_context_data(**kwargs)
+        self.category = get_object_or_404(Category, pk=self.kwargs['pk'])
+        context['category'] = self.category
+        return context
